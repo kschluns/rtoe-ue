4910d3a2d224620c175e177350484ad72535737c
kschluns <kschluns@bu.edu>
2026-01-05 13:45:15 -0600

    satcat: include today in partitions

--- src/rtoe_ue/defs/partitions/dates.py
+++ src/rtoe_ue/defs/partitions/dates.py
@@ -4,7 +4,17 @@ import os
 from dagster import DailyPartitionsDefinition
 
 # Partition key is the ingestion date: "YYYY-MM-DD"
-INGESTION_DATE_PARTITIONS = DailyPartitionsDefinition(start_date="2026-01-01")
+INGESTION_DATE_PARTITIONS = DailyPartitionsDefinition(
+    start_date="2026-01-01",
+    timezone="America/Chicago",
+    # include "today" as a valid partition key
+    end_offset=1,
+)
 
 # You’ll use this later for GP history creation_date partitions
-CREATION_DATE_PARTITIONS = DailyPartitionsDefinition(start_date="2025-01-01")
+CREATION_DATE_PARTITIONS = DailyPartitionsDefinition(
+    start_date="2025-01-01",
+    timezone="America/Chicago",
+    # include "today" as a valid partition key
+    end_offset=1,
+)
